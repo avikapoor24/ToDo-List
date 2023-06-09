3a73211daebc0e3893b224358130eab0b40576f6
avikapoor24 <avikapoor244@gmail.com>
2023-06-09 21:26:06 +0530

    commit
    Signed-off-by: avikapoor24 <avikapoor244@gmail.com>

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -8,21 +8,35 @@ export default function ToDoList() {
     const [todos, setTodos] = useState([]);
    
      //Add Todo to list
-    const addTask = task =>
-    {
+    const addTask = task => {
         if (!task.text) {
             return
         }
         const newTodos = [task, ...todos];
         setTodos(newTodos);
-        }
-    //remove Todo from list
+    }
 
+    //remove Todo from list
+    const removeTask = id => {
+        let updatedTasks = [...todos].filter(task => task.id !== id)
+        setTodos(updatedTasks)
+    }
     //task is completed
-    
+    const completedTask = id => {
+        let updatedTasks = todos.map(todo =>
+        {
+            if (todo.id === id) {
+                todo.isComplete = true;
+            }
+            return todo;
+        })
+        setTodos(updatedTasks)
+    }
+
   return (
       <div>
           <ToDoForm addTask={addTask}></ToDoForm>
+          <Todo todos={todo} completedTask={completedTask} removeTask={removeTask} ></Todo>
     </div>
   )
 }

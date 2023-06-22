67c51d228cb85f9163f2fe50bdb789a92768b816
avikapoor24 <avikapoor244@gmail.com>
2023-06-23 00:05:25 +0530

    Final commit
    Signed-off-by: avikapoor24 <avikapoor244@gmail.com>

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,42 +1,17 @@
-import React from 'react';
-import { useState } from 'react';
-import ToDoForm from './ToDoForm'
+import React from 'react'
 
-export default function ToDoList() {
-
-    //state that holds all Todos
-    const [todos, setTodos] = useState([]);
-   
-     //Add Todo to list
-    const addTask = task => {
-        if (!task.text) {
-            return
-        }
-        const newTodos = [task, ...todos];
-        setTodos(newTodos);
-    }
-
-    //remove Todo from list
-    const removeTask = id => {
-        let updatedTasks = [...todos].filter(task => task.id !== id)
-        setTodos(updatedTasks)
-    }
-    //task is completed
-    const completedTask = id => {
-        let updatedTasks = todos.map(todo =>
-        {
-            if (todo.id === id) {
-                todo.isComplete = true;
-            }
-            return todo;
-        })
-        setTodos(updatedTasks)
-    }
-
+function Todolist(props) {
   return (
-      <div>
-          <ToDoForm addTask={addTask}></ToDoForm>
-          <Todo todos={todo} completedTask={completedTask} removeTask={removeTask} ></Todo>
-    </div>
+    <li className="list-item">
+        {props.item}
+        <span className='icons'>
+        <i className="fa-solid fa-trash-can icon-delete" 
+        onClick={e=>{
+            props.deleteItem(props.index)
+        }}></i>
+        </span>
+    </li>
   )
 }
+
+export default Todolist
